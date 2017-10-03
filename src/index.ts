5222ef40080750f172d87483be97c28c37cdc094
Arijit Dasgupta <arijit.rivalslayer@gmail.com>
2017-10-03 19:50:08 +0200

    Minor refactors

--- src/index.ts
+++ src/index.ts
@@ -18,27 +18,29 @@ app.set('port', NodePort);
 app.use(expressLogger(logger));
 app.use(cors({origin: true}));
 
-const usersController = container.get<interfaces.IController>(TYPES.UsersController);
-const itemsController = container.get<interfaces.IController>(TYPES.ItemsController);
-const transcationsController = container.get<interfaces.IController>(TYPES.TransactionsController);
-
-const ioHalter = container.get<IOHalter>(TYPES.IOHalter);
-
 const initApplication = () => {
-    app.use(usersController.application);
-    app.use(itemsController.application);
-    app.use(transcationsController.application);
-
-    // Just a status endpoint
-    app.get('/status', (request:express.Request, response:express.Response) => {
-        response.send('OK');
-    });
-
     app.listen(app.get('port'), () => {
         console.log(`Application is listening on PORT ${app.get('port')}`)
     });
 };
 
+const usersController = container.get<interfaces.IController>(TYPES.UsersController);
+const itemsController = container.get<interfaces.IController>(TYPES.ItemsController);
+const transcationsController = container.get<interfaces.IController>(TYPES.TransactionsController);
+
+app.use(usersController.application);
+app.use(itemsController.application);
+app.use(transcationsController.application);
+
+// Just a status endpoint
+app.get('/status', (request:express.Request, response:express.Response) => {
+    response.send('OK');
+});
+
+const ioHalter = container.get<IOHalter>(TYPES.IOHalter);
+
 Promise.all(ioHalter.getAllPromises()).then(initApplication);
+
+
 
 
